c89096d38738c30adaab3bd250dcb16bd5f4dcc9
Ravi Ranjan <raviranjan9601@gmail.com>
2025-11-26 16:53:02 +0530

    first commit

---
+++ Sum of n Natural Numbers without using scanf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main() {
+    int n = 10;
+    int i, sum = 0;
+    for(i = 1; i <= n; i++) {
+        sum += i;
+    }
+    printf("Sum of first %d natural numbers = %d\n", n, sum);
+    return 0;
+}
---
+++ sum of n odd numbers without using scanf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main() {
+    int n = 5;
+    int sum = 0;
+    for(int i = 1; i <= n; i++) {
+        sum += (2 * i - 1);
+    }
+    printf("Sum of first %d odd numbers = %d\n", n, sum);
+    return 0;
+}
