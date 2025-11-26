6f1ac75693b60abb435558f71e76aaa8dc329108
Ravi Ranjan <raviranjan9601@gmail.com>
2025-11-26 21:04:47 +0530

    add new Factorial

---
+++ Factorial.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+    int n = 5;
+    long long fact = 1;
+    for (int i = 1; i <= n; i++) {
+        fact *= i;
+    }
+    printf("Factorial of %d = %lld\n", n, fact);
+
+    return 0;
+}
